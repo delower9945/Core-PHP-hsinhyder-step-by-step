952ecdaaab9ffe9e93f35e0c50d9336013c255f9
delower9945 <delowerweb99@gmail.com>
2021-02-12 01:53:34 -0800

    tarnary oparator

--- leap-year.php
+++ leap-year.php
@@ -4,21 +4,27 @@
 // 2 divisible by 100
 // 3 divisible by 400
 
-$year = 2016;
-if($year % 4 && $year % 100 ==0 && $year % 400 ==0){
-    echo "{$year} is a leap year";
-}
-elseif($year % 4==0){
-    echo "{$year} is leap year";
-}
-else{
-    echo "{$year} in not leap year";
-}
+// $year = 2016;
+// if($year % 4 && $year % 100 ==0 && $year % 400 ==0){
+//     echo "{$year} is a leap year";
+// }
+// elseif($year % 4==0){
+//     echo "{$year} is leap year";
+// }
+// else{
+//     echo "{$year} in not leap year";
+// }
 
-// Read able Advance ---------
+// // Read able Advance ---------
 
-if($year % 4==0 &&($year% 100 !=0 || $year % 400==0)){
-    echo "is a leapear";
-}else {
-    echo "is a not lepear";
-}
+// if($year % 4==0 &&($year% 100 !=0 || $year % 400==0)){
+//     echo "is a leapear";
+// }else {
+//     echo "is a not lepear";
+// }
+
+// ternary Operator start 
+$n=15;
+$number = 12 == $n ? "ok" : "not Ok";
+
+echo $number;
